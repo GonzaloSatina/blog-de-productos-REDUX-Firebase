d013a187fc7ce20793e7fa38ed53de84482f3bc4
Gonzalo Satina Naggi <gonzalosatina@gmail.com>
2022-07-05 11:19:15 -0300

    1

---
+++ firebase/config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWLM3x92-2Gs-bRQqI4z16ysvsWORnKlc",
+
+  authDomain: "product-hunt-433c7.firebaseapp.com",
+
+  projectId: "product-hunt-433c7",
+
+  storageBucket: "product-hunt-433c7.appspot.com",
+
+  messagingSenderId: "1058354698844",
+
+  appId: "1:1058354698844:web:06db8d4687f78ad5812499",
+};
+
+export default firebaseConfig;
